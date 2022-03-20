08f9b394f0c04f7e74092a038bf5281bb9f15afb
Vanessa Soares de Morais <vsmdantas@outlook.com>
2022-03-20 08:53:43 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+namespace tipos_de_variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            var x = 1;
+            Console.WriteLine(i);
+            Console.WriteLine(x);
+
+            //i = "s";
+            //x = "333";
+
+        }
+    }
+}
